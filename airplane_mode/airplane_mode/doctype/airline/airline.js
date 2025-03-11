51cfda5bc345c0f1a8c5998595ca8ccd4f70d6af
iphenelist <innocentphenelist@gmail.com>
2025-03-11 17:08:17 +0300

    feat: fourth commit

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, innocent metumba and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airline", {
+  refresh(frm) {
+      if (frm.doc.website) {
+          frm.add_web_link(__('Visit Website'), frm.doc.website);
+      }
+  },
+  website: function(frm) {
+      if (frm.doc.website && !frm.doc.website.startsWith('https://')) {
+          frm.set_value('website', 'https://' + frm.doc.website);
+      }
+  }
+});
--- airplane_mode/airplane_mode/doctype/airplane/airplane.js
+++ airplane_mode/airplane_mode/doctype/airplane/airplane.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, innocent metumba and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane", {
+    refresh: function(frm) {
+        if (!frappe.user_role.includes("Airport Authority Personnel")) {
+            frm.set_df_property("initial_audit_completed", "hidden", 1);
+            frm.set_df_property("initial_audit_completed", "read_only", 1);
+        } else {
+            frm.set_df_property("initial_audit_completed", "hidden", 0);
+        }
+    }
+});
 
-// 	},
-// });
